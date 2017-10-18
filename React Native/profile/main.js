c2e89023575bf260199e7dc744070a8ee7deb9c4
Zheng Hanrong <hanrongzheng@gmail.com>
2017-10-18 23:19:15 +0800

    尝试react-native-fs模块

--- React Native/profile/main.js
+++ React Native/profile/main.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import {View, Text} from "react-native"
 import url from "../base/url"
+import RNFS from "react-native-fs"
 class Main extends Component {
 
     constructor(props) {
@@ -8,11 +9,17 @@ class Main extends Component {
         this.state={profile:""}
     }
     componentWillMount() {
+
+
         fetch(url.profile+"?uid=32953014", {
             method:"GET"
         })
             .then(res=> JSON.stringify(res))
-            .then(res=>this.setState({profile:res}))
+            .then(res=>{this.setState({profile:res})
+            RNFS.writeFile( "json.txt", res , "utf8")
+                .then(yes=>alert("写入文件成功"))
+                .catch(err=>alert("写入失败"+err))
+            })
             .catch(err=>alert(err))
     }
     render() {
